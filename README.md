f45ba8ffb673b6781ce3e6560bd79b37bab7b2fa
aizenLM <diamnteludwing145@gmail.com>
2024-09-25 12:04:15 -0600

    new branch

---
+++ README.md
@@ -0,0 +1,9 @@
+<center>
+    <img src="./md/images/ITGAMBanner.png" alt="itgam Banner">
+<center>
+# Shipping Cart 🛒 Project
+Proyecto didactico para el cuerso de 
+Desarrollo de apps
+
+## Autor 
+Enrique Ortega Celis 💀
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+// Modelo
+const header = ref('App lista de compras')
+const itemsList2 = ref([
+  {
+    'id': 101,
+    'name': '10 bolillos',
+  },
+
+  {
+    'id': 102,
+    'name': '1 lata volt',
+  },
+  {
+    'id': 103,
+    'name': '1 bote de cafe',
+  },
+
+  {
+    'id': 104,
+    'name': 'chetos flaming hot',
+  }
+])
+</script>
+
+<template>
+  <h1>
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+  </h1>
+  <ul>
+    <li v-for="({id, name}, i) in itemsList2" :key="id">{{i + 1}} {{name}}</li>
+  </ul>
+</template>
+<style scoped>
+.shopping-cart-icon {
+  font-size: 2rem;
+}
+</style>
